8158f55ade36a51d8e080fe9cb6abf0ea8c2835d
anmol goel <anmolgoyal74@gmail.com>
2018-02-27 18:00:29 +0530

    import node

--- src/Form/NodeExportForm.php
+++ src/Form/NodeExportForm.php
@@ -15,6 +15,7 @@ use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityInterface;
+
 /**
  * Provides a Node Export form.
  */
@@ -33,18 +34,46 @@ class NodeExportForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state) {
     $nid = \Drupal::routeMatch()->getParameter('node');
     //$nid = $node->nid->value;
+
+
     $node = \Drupal\node\Entity\Node::load($nid);
-    print_r($node);
-    die();
-    ob_start();
-    var_dump($node);
-    $result = ob_get_clean();
-  
+    // print_r($node);
+    // die();
+    $result=array();
+    foreach ($node as $key=>$value) {
+      $result[$key]=$node->get($key)->getValue()[0];
+
+      // if($key=='body')
+      // {
+      //   $result['format']=$node->get($key)->getValue()[0]['format'];
+      //   $result['body']=$node->get($key)->getValue()[0]['value'];
+      // }
+      // else if($key=='type')
+      // {
+      // $result[$key]=$node->get($key)->getValue()[0]['target_id'];
+      // }
+      // else if($key=='field_image')
+      // {
+      // $result[$key]=$node->get($key)->getValue()[0];
+      // }
+      // else
+      // $result[$key]=$node->get($key)->getValue()[0]['value'];
+    }
+
+     $json=json_encode($result);
+     //  print_r($json);
+     // die();
+
+    // ob_start();
+    // var_dump($node);
+    // $result = ob_get_clean();
+    //print_r($result);
+    //die();
     $form['export_code'] = [
       '#type' => 'textarea',
-      '#value' => $result,
+      '#value' => $json,
       '#title' => t('Node Export Code is :'),
-      '#cols' => '50',
+      '#rows' => '15',
     ];
 
     $form['submit'] = array(
--- src/Form/NodeImportForm.php
+++ src/Form/NodeImportForm.php
@@ -5,16 +5,18 @@
  */
 namespace Drupal\node_Export\Form;
 
+use Drupal\node\Plugin\views\argument_default;
 use Drupal\Core\Database\Database;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\Entity;
 use Drupal\node\Entity\Node; 
+use Drupal\Core\Entity;
 use Drupal\Core\Access\AccessibleInterface;
 use Drupal\Core\Cache\CacheableDependencyInterface;
 use Drupal\Core\Entity\ContentEntityBase;
 use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityInterface;
+
 /**
  * Provides a Node Import form.
  */
@@ -35,11 +37,10 @@ class NodeImportForm extends FormBase {
     $form['paste'] = array(
     '#type' => 'textarea',
     '#default_value' => '',
-    '#rows' => 30,
+    '#rows' => 15,
     '#description' => t('Paste the code of a node export here.'),
     '#wysiwyg' => FALSE,
   );
-
   $form['actions'] = array('#type' => 'actions');
   $form['actions']['submit'] = array(
     '#type' => 'submit',
@@ -48,22 +49,44 @@ class NodeImportForm extends FormBase {
     return $form;
   }
 
-
-
   /**
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) { 
-      $node=$form_state->getValue('paste');
+      $json=$form_state->getValue('paste');
 
-      //$new = clone($node);
-      $node->is_new = true;
-      die();
-      unset($node->nid);
-      unset($node->vid);
-      unset($node->tnid);
+      $node=json_decode($json,true);
+  //     $node2 = Entity::create('node', array(
+  //        'type' => 'new_content_type',
+  //        'title' =>'Creating another node',
+  //        'body' => array(
+  //          'value' =>'The body of the content',
+  //          'format' => 'full_html',
+  //            ),
+  //        'field_mail'=>'email@gmail.com',
+  //        'field_link'=>'http://www.example.com',
+  //        'field_date'=>[ '2017-07-22'],
+  //        )
+  // );
+print_r($node['comment']);
+die();
+      $nodenew = Node::create([
+        'type'        => $node['type']['target_id'],
+        'title'       => $node['title'],
+        'body'        => $node['body'],
+        'field_image' => $node['field_image'],
+        'field_tags' => $node['field_tags'],
+        'comment' => $node['comment'],
+      ]);
+      $nodenew->save();
+      // print_r($node->title->value);
+      // print_r($node->type->value);
+      // $node->is_new = true;
+      // unset($node->nid);
+      // unset($node->vid);
+      // unset($node->tnid);
       
-      node_save($node);
+     // node_save($node);
 
     drupal_set_message(t('Node Content Type has been changed succesfully.'));
   }
